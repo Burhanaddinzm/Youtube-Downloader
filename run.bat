c807572d83e94f0c025b0dbbea3060815995edd2
Burhanaddinzm <burhanaddinzm@code.edu.az>
2024-08-02 14:25:50 +0400

    Major changes
    Got rid of url.txt file for taking user input instead takes the url from args. Added option for only downloading video or audio. Improved error handling

--- run.bat
+++ run.bat
@@ -1,9 +1,29 @@
 @echo off
 
+:: Prompt for the URL
 set /p url=Enter URL:
-echo "%url%" > url.txt
 
-node ./server.js
-echo Download completed! Press any key to open Downloads directory.
-pause
-explorer Downloads
+:: Prompt for the option
+set /p option=Enter option v(video), a(audio) and empty for both:
+
+:: Handle empty option
+if "%option%" == "" (
+  set option=null
+)
+
+:: Run the Node.js script
+node ./server.js "%url%" %option%
+
+:: Check if the Node.js script was successful
+if %errorlevel% == 0 (
+  echo Download completed! Press any key to open Downloads directory.
+  pause
+
+  :: Open the Downloads directory
+  explorer Downloads
+) else (
+  echo Download failed!
+  pause
+)
+
+
--- server.js
+++ server.js
@@ -1,49 +1,54 @@
-const fs = require("fs");
 const crypto = require("crypto");
 const path = require("path");
 const youtubedl = require("youtube-dl-exec");
 const progressEstimator = require("progress-estimator");
 
 const downloadsDir = path.join(__dirname, "Downloads");
-
 const logger = progressEstimator();
+
+const url = process.argv[2];
+const option = process.argv[3] === "null" ? null : process.argv[3];
 
 function guidGen() {
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-    (
-      +c ^
-      (crypto.randomBytes(1)[0] & (15 >> (+c / 4)))
-    ).toString(16)
+    (+c ^ (crypto.randomBytes(1)[0] & (15 >> (+c / 4)))).toString(16)
   );
 }
 
-async function downloadVideo() {
+async function downloadVideo(url, option) {
   try {
-    const url = await fs.promises.readFile("./url.txt", "utf8");
-    const trimmedUrl = url.replace('"', "").trim();
+    const trimmedUrl = url.trim();
 
     const videoInfoPromise = youtubedl(trimmedUrl, {
       dumpSingleJson: true,
       noCheckCertificate: true,
       noWarnings: true,
       preferFreeFormats: true,
-      addHeader: [
-        "referer:youtube.com",
-        "user-agent:googlebot",
-      ],
+      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
     });
 
     const videoInfo = await logger(videoInfoPromise, `Obtaining ${trimmedUrl}`);
-    const format = videoInfo.formats.find(
-      (f) => f.vcodec !== "none" && f.acodec !== "none"
-    );
 
-    if (!format) {
-      console.log("No video formats found");
-      return;
+    // Get format
+    let format;
+
+    if (!option) {
+      format = videoInfo.formats.find(
+        (f) => f.vcodec !== "none" && f.acodec !== "none"
+      );
+    } else if (option === "v") {
+      format = videoInfo.formats.find((f) => f.vcodec !== "none");
+    } else if (option === "a") {
+      format = videoInfo.formats.find((f) => f.acodec !== "none");
     }
 
-    const filePath = path.join(downloadsDir, `${guidGen()}.mp4`);
+    if (!format) {
+      throw new Error("No suitable formats found!");
+    }
+
+    const extension = option === "a" ? "mp3" : "mp4";
+
+    const filePath = path.join(downloadsDir, `${guidGen()}.${extension}`);
     const downloadPromise = youtubedl.exec(trimmedUrl, {
       format: format.format_id,
       output: filePath,
@@ -52,8 +57,9 @@ async function downloadVideo() {
     await logger(downloadPromise, `Downloading ${filePath}`);
     console.log("Download complete:", filePath);
   } catch (err) {
-    console.error("Error:", err);
+    console.error("Error:", err.message);
+    process.exit(1);
   }
 }
 
-downloadVideo();
+downloadVideo(url, option);
